5c25844bc8866e7fae8def6a142f3fa606508e6d
Dimon713 <Dimon713@tut.by>
2020-06-03 16:44:06 +0300

    03.06.2020

--- Practical task №6 (pow and calculate)/pow-and-calculate.js
+++ Practical task №6 (pow and calculate)/pow-and-calculate.js
@@ -49,3 +49,38 @@ function carryCalc(f) {
         }
     }
 }
+
+let pow = carryPowers(powers);
+let calculate = carryCalc(calc);
+
+console.log(`1) Вызов:
+   pow(-2)(3);
+   calculate(1)("+")(2));
+   Результат:
+   -2^3=${pow(-2)(3)}
+    1+2=${calculate(1)("+")(2)}
+`);
+
+console.log(`2) Вызов:
+   pow(4)(2);
+   calculate(3)("*")(7));
+   Результат:
+   4^2=${pow(4)(2)}
+   3*7=${calculate(3)("*")(7)}
+`);
+
+console.log(`3) Вызов:
+   pow(16)(0);
+   calculate(15)("/")(3));
+   Результат:
+   16^0=${pow(16)(0)}
+   15/3=${calculate(15)("/")(3)}
+`);
+
+console.log(`4) Вызов:
+   pow(2)(-3);
+   calculate(2)("/")(0));
+   Результат:
+   2^(-3)=${pow(2)(-3)}
+   2/0=${calculate(2)("/")(0)}
+`);
