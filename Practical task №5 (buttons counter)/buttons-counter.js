914a4db7f44a744de1350484d1028a8f0d8c9e99
Dimon713 <Dimon713@tut.by>
2020-06-03 15:24:14 +0300

    03.06.2020

--- Practical task №5 (buttons counter)/buttons-counter.js
+++ Practical task №5 (buttons counter)/buttons-counter.js
@@ -1,21 +1,13 @@
-let buttonOne = document.getElementById("one");
-let buttonTwo = document.getElementById("two");
-let buttonThree = document.getElementById("three");
-
 function makeCounter() {
-    let count = 0;
-    return function(e) {
-        count++;
-        e.target.innerHTML = count;
+    let counter = 0;
+    return () => {
+        return ++counter;
     }
-}
+};
 
-let counterForButtonOne = makeCounter();
-let counterForButtonTwo = makeCounter();
-let counterForButtonThree = makeCounter();
-
-buttonOne.onclick = counterForButtonOne;
-buttonTwo.onclick = counterForButtonTwo;
-buttonThree.onclick = counterForButtonThree;
-
-// <button id="one" onclick="counterForButtonOne()">0</button> <--Такой вариант не работал, событие не передавалось. Уточнить.
+document.querySelectorAll(".btn").forEach(element => {
+    let count = makeCounter();
+    element.onclick = () => {
+        element.innerText = count();
+    };
+});
